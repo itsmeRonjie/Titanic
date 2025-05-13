313d26b7254182dc7a2d87ef7d833845ca6374f1
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-13 14:11:30 +0800

    - initialized project

---
+++ Titanic/TitanicApp.swift
@@ -0,0 +1,17 @@
+//
+//  TitanicApp.swift
+//  Titanic
+//
+//  Created by Ronjie Diafante Man-on on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct TitanicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
